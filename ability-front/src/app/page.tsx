ba9ec47720a36f874068698339387452b0a64efb
k2madureira <lenilsonmadureira@gmail.com>
2023-05-30 17:20:32 -0300

    feat: dashboard

--- ability-front/src/app/page.tsx
+++ ability-front/src/app/page.tsx
@@ -1,3 +1,12 @@
+import Link from 'next/link';
+
 export default function Home() {
-	return <h1>Home</h1>;
+	return (
+		<>
+			<h1>Home</h1>
+			<div>
+				<Link href="/dashboard">DASHBOARD</Link>
+			</div>
+		</>
+	);
 }
