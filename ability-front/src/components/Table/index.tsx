017594e5f83ad9a66bbb1e668cde9ce5f73c7d38
Lenilson-ioasys <lenilsonmadureira@ioasys.com.br>
2023-06-19 20:58:55 -0300

    feat: adicionado tabelas

--- ability-front/src/components/Table/index.tsx
+++ ability-front/src/components/Table/index.tsx
@@ -1,23 +1,154 @@
 'use client';
+import {useReactTable, createColumnHelper, flexRender, getCoreRowModel} from '@tanstack/react-table';
 import { FaUserEdit, FaPowerOff, FaRegStar } from 'react-icons/fa';
 import * as S from "./styles";
-import { useContext } from 'react';
-import { AuthContext } from '@/contexts/AuthContext';
+import { useReducer, useState } from 'react';
 
 interface IProps {
   name: string;
 }
 
-export function Table({ name }:IProps){
-  const { signOut } = useContext(AuthContext);
-  async function handleSignOut() {
-		await signOut();
-	}
 
+type Student = {
+  name: string;
+  group: string;
+  instrument: string;
+};
+
+const studentData: Student[] = [
+  {
+    name: 'Lenilson 1111111111111111111',
+    group: 'Adulto',
+    instrument: 'Violino'
+  },
+  {
+    name: 'Samuel',
+    group: 'Infantil',
+    instrument: 'Violino'
+  },
+  {
+    name: 'Lenilson 1111111111111111111',
+    group: 'Adulto',
+    instrument: 'Violino'
+  },
+  {
+    name: 'Samuel',
+    group: 'Infantil',
+    instrument: 'Violino'
+  },
+  {
+    name: 'Lenilson 1111111111111111111',
+    group: 'Adulto',
+    instrument: 'Violino'
+  },
+  {
+    name: 'Samuel',
+    group: 'Infantil',
+    instrument: 'Violino'
+  },
+  {
+    name: 'Lenilson 1111111111111111111',
+    group: 'Adulto',
+    instrument: 'Violino'
+  },
+  {
+    name: 'Samuel',
+    group: 'Infantil',
+    instrument: 'Violino'
+  },
+  {
+    name: 'Lenilson 1111111111111111111',
+    group: 'Adulto',
+    instrument: 'Violino'
+  },
+  {
+    name: 'Samuel',
+    group: 'Infantil',
+    instrument: 'Violino'
+  },
+  {
+    name: 'Lenilson 1111111111111111111',
+    group: 'Adulto',
+    instrument: 'Violino'
+  },
+  {
+    name: 'Samuel',
+    group: 'Infantil',
+    instrument: 'Violino'
+  }
+];
+
+const headers = ['Name','Group','Instrument'];
+const columnStudentHelper = createColumnHelper<Student>();
+
+const columnsStudent = [
+  columnStudentHelper.accessor(row => row.name, {
+    id: 'Name',
+    cell: info => <i>{info.getValue()}</i>,
+    header: () => 'Name',
+    footer: info => info.column.id,
+  }),
+  columnStudentHelper.accessor(row => row.group, {
+    id: 'Group',
+    cell: info => <i>{info.getValue()}</i>,
+    header: () => 'Group',
+    footer: info => info.column.id,
+  }),
+  columnStudentHelper.accessor(row => row.instrument, {
+    id: 'Instrument',
+    cell: info => <i>{info.getValue()}</i>,
+    header: () => 'Instrument',
+    footer: info => info.column.id,
+  }),
+];
+
+
+
+export function Table({ name }:IProps){
+  const [data, setData] = useState(() => [...studentData])
+  const rerender = useReducer(() => ({}), {})[1]
+
+  const table = useReactTable({
+    data: studentData,
+    columns: columnsStudent,
+    getCoreRowModel: getCoreRowModel(),
+  })
+console.log({ ...table.getRowModel() })
   return (
     <div className='grid-content-area'>
       <S.Table tabIndex={0}>
-          TABELA
+      <table>
+        <thead>
+          {table.getHeaderGroups().map((headerGroup, index) => (
+            <tr key={headerGroup.id}>
+              {headerGroup.headers.map(header => (
+                <th key={header.id} className={header.id !== 'Name'? 'hidden-info':''}>
+                  {header.isPlaceholder
+                    ? null
+                    : flexRender(
+                        header.column.columnDef.header,
+                        header.getContext()
+                      )}
+                </th>
+              ))}
+              <th>Options</th>
+            </tr>
+          ))}
+        </thead>
+        <tbody>
+          {table.getRowModel().rows.map(row => (
+            <tr key={row.id}>
+              {row.getVisibleCells().map((cell, index) => (
+                <td key={cell.id} data-th={headers[index]} className={index !== 0? 'hidden-info':''}>
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </td>
+              ))}
+              <td data-th="Options">OPTIONS</td>
+            </tr>
+          ))}
+        </tbody>
+  
+      </table>
       </S.Table>
     </div>
   );
--- ability-front/src/components/Table/styles.ts
+++ ability-front/src/components/Table/styles.ts
@@ -27,12 +27,84 @@ export const StyledLink = styled(Link)`
   }
 `;
 
-export const TABLE_DESKTOP = "14vw";
-export const TABLE_TABLET = "20vw";
-export const TABLE_MOBILE = "28vw";
+export const TABLE_MOBILE = "76vw";
+export const TABLE_TABLET = "75vw";
+export const TABLE_DESKTOP = "75vw";
 
 export const Table = styled.div`
+  width: ${TABLE_MOBILE};
+  height: 36vh;
+  overflow-y: auto;
+
+  table {
+    margin: auto;
+    min-width: ${TABLE_MOBILE};
+    max-width: 90%;
+    border-collapse: collapse;
+    color: var(--black);
+    border-radius: 0.4em;
+    overflow: hidden;
+    border-spacing: 0.5rem;
+    border-collapse: separate;
+
+    thead {
+      background-color: var(--fchisia-800);
+      color: var(--white);
+
+      tr {
+        th {
+          padding: 0.5rem;
+        }
+      }
+    }
+
+    tbody {
+      tr {
+        td {
+          border: 1px solid var(--fchisia-800);
+          padding: 0.5rem;
+          font-size: 0.8rem;
+        }
+      }
+    }
+  }
+
+  .hidden-info {
+    display: none;
+  }
+
   @media (min-width: 740px) {
+    .hidden-info {
+      display: inline;
+    }
+
+    table tr:nth-child(2) {
+      border-top: none;
+    }
+
+    table tr:hover:not(:first-child) {
+      background-color: #d8e7f3;
+    }
+    table td:before {
+      display: none;
+    }
+    table th,
+    table td {
+      display: table-cell;
+      padding: 0.25em 0.5em;
+    }
+    table th:first-child,
+    table td:first-child {
+      padding-left: 0;
+    }
+    table th:last-child,
+    table td:last-child {
+      padding-right: 0;
+    }
+    table th,
+    table td {
+      padding: 1em !important;
+    }
   }
 
   @media (min-width: 1024px) {
